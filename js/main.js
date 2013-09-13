34c831a9c04d8784f7d4733b6178376be459ead2
Drew Ditthardt <drew.ditthardt@gmail.com>
2013-09-12 17:10:28 -0700

    updated things

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-$('.nav a').on('click', function(){
+$(function(){
+  $('.nav a').on('click', function(){
     $(".navbar-toggle").click();
-});
+  });
 
 
-$('#map').gmap3({
-  map: {
-    options: {
-      maxZoom: 14
+  $('#map').gmap3({
+    map: {
+      options: {
+        maxZoom: 14
+      }
+    },
+    marker: {
+      latLng: [33.850628,-84.713793],
+      //address: "5436 HILL ROAD. POWDER SPRINGS, GA. 30127",
+      options: {}
     }
   },
-  marker: {
-    latLng: [33.850628,-84.713793],
-    //address: "5436 HILL ROAD. POWDER SPRINGS, GA. 30127",
-    options: {}
-  }
-},
-"autofit" );
+  "autofit" );
+}
